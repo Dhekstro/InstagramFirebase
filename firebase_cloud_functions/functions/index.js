426c273c9e87fd51bc2cf16071ff866bb148fc36
Brian Voong <bhlvoong@gmail.com>
2017-11-30 12:40:47 -0800

    Ep 45 Observe User Cloud Functions

--- firebase_cloud_functions/functions/index.js
+++ firebase_cloud_functions/functions/index.js
@@ -17,31 +17,50 @@ exports.sendPushNotifications = functions.https.onRequest((req, res) => {
 
   // admin.message().sendToDevice(token, payload)
 
-  var fcmToken = "eeRQjRMMp0A:APA91bFolriS7pxo0XumRe0gdnqxIn18FPh0kAC-3EDhUEeLm1_9Whz4qbzwjSMIXyn46k3F37lfZq24i2Pm1q1xsF67kVSbRS08C_kTusFZqbqRPWOGv7hbGi0SqKrLfV-YOgTdE5bO";
+  var uid = 'NUPR2dHs7ehUYN01PtzKpyJbY753';
+
+  return admin.database().ref('/users/' + uid).once('value', snapshot => {
+
+    var user = snapshot.val();
+
+    console.log("User username: " + user.username + " fcmToken: " + user.fcmToken);
+
+    var payload = {
+      notification: {
+        title: "Push notification TITLE HERE",
+        body: "Body over here is our message body..."
+      }
+    }
+
+    admin.messaging().sendToDevice(user.fcmToken, payload)
+      .then(function(response) {
+        // See the MessagingDevicesResponse reference documentation for
+        // the contents of response.
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function(error) {
+        console.log("Error sending message:", error);
+      });
+
+  })
+
+  // var fcmToken = "eeRQjRMMp0A:APA91bFolriS7pxo0XumRe0gdnqxIn18FPh0kAC-3EDhUEeLm1_9Whz4qbzwjSMIXyn46k3F37lfZq24i2Pm1q1xsF67kVSbRS08C_kTusFZqbqRPWOGv7hbGi0SqKrLfV-YOgTdE5bO";
 
   // See the "Defining the message payload" section below for details
   // on how to define a message payload.
-  var payload = {
-    notification: {
-      title: "Push notification TITLE HERE",
-      body: "Body over here is our message body..."
-    },
-    data: {
-      score: "850",
-      time: "2:45"
-    }
-  };
+  // var payload = {
+  //   notification: {
+  //     title: "Push notification TITLE HERE",
+  //     body: "Body over here is our message body..."
+  //   },
+  //   data: {
+  //     score: "850",
+  //     time: "2:45"
+  //   }
+  // };
+  //
+  // // Send a message to the device corresponding to the provided
+  // // registration token.
 
-  // Send a message to the device corresponding to the provided
-  // registration token.
-  admin.messaging().sendToDevice(fcmToken, payload)
-    .then(function(response) {
-      // See the MessagingDevicesResponse reference documentation for
-      // the contents of response.
-      console.log("Successfully sent message:", response);
-    })
-    .catch(function(error) {
-      console.log("Error sending message:", error);
-    });
 
 })
