bc0e8cb6011fec10038ce8ce4347fd270d15257c
Brian Voong <bhlvoong@gmail.com>
2017-11-30 16:23:16 -0800

    Ep 46 User Follow Cloud Trigger

--- firebase_cloud_functions/functions/index.js
+++ firebase_cloud_functions/functions/index.js
@@ -11,6 +11,46 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase LBTA!");
 });
 
+// listen for Following events and then trigger a push notification
+exports.observeFollowing = functions.database.ref('/following/{uid}/{followingId}')
+  .onCreate(event => {
+
+    var uid = event.params.uid;
+    var followingId = event.params.followingId
+
+    // let's log out some messages
+
+    console.log('User: ' + uid + ' is following: ' + followingId);
+
+    // trying to figure out fcmToken to send a push message
+    return admin.database().ref('/users/' + followingId).once('value', snapshot => {
+
+      var userWeAreFollowing = snapshot.val();
+
+      return admin.database().ref('/users/' + uid).once('value', snapshot => {
+
+        var userDoingTheFollowing = snapshot.val();
+
+        var payload = {
+          notification: {
+            title: "You now have a new follower",
+            body: userDoingTheFollowing.username + ' is now following you'
+          }
+        }
+
+        admin.messaging().sendToDevice(userWeAreFollowing.fcmToken, payload)
+          .then(response => {
+            console.log("Successfully sent message:", response);
+          }).catch(function(error) {
+            console.log("Error sending message:", error);
+          });
+
+      })
+
+
+    })
+  })
+
 exports.sendPushNotifications = functions.https.onRequest((req, res) => {
   res.send("Attempting to send push notification...")
   console.log("LOGGER --- Trying to send push message...")
